7662ff7dda8142afacb2cbe983eb388857c20ccf
Mayank Shukla <117716383+mayankshukla-20@users.noreply.github.com>
2023-12-18 11:07:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+function mapLetters(str){
+	let ans = {};
+	for(let i=0;i<str.length;i++){
+		if(ans[str[i]]){
+			ans[str[i]].push(i);
+		}
+		else{
+			ans[str[i]] = [i];
+		}
+	}
+	return ans;
+}
+
+const str = propmt("Enter the string");
+alert(mapLetters(str));
